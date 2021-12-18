077b87ec91ef667df9693a6d70a2ba798128a5e3
k.cherkalov <kirill.cherkalov@sdventures.com>
2021-12-19 01:10:14 +0300

    initial

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import {runOnJS} from 'react-native-reanimated';
+
+import {StyleSheet} from 'react-native';
+import {useCameraDevices, useFrameProcessor} from 'react-native-vision-camera';
+
+import {Camera} from 'react-native-vision-camera';
+import {Face} from 'vision-camera-face-detector';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [faces, setFaces] = React.useState<Face[]>();
+
+  const devices = useCameraDevices();
+  const device = devices.front;
+
+  React.useEffect(() => {
+    console.log(faces);
+  }, [faces]);
+
+  React.useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+    const scannedFaces = __scanFaces(frame);
+    runOnJS(setFaces)(scannedFaces);
+  }, []);
+
+  return device != null && hasPermission ? (
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+      frameProcessorFps={5}
+    />
+  ) : null;
+}
